f3965d5fa5f6b1d44fb0a1a3877f197476f74725
Renaud <charlierrenaud@hotmail.com>
2024-02-22 18:59:35 +0200

    updates

---
+++ incr-backup_2.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# Incremental Backup Script
+# -----------------------------------------------------------------------------
+# Author: Renaud CHARLIER
+# Date: 18-02-2024
+# Description: Performs an incremental backup using rsync, keeping backups
+# from the specified number of days and ensuring at least the two most recent
+# backups are always retained.
+# Usage: ./backup_script.sh <TARGET_DIR> <RETENTION>
+# Example: ./backup_script.sh TICKETS 7
+# Note: Ensure SSH password-less authentication is set up for rsync_adm@backup-srv.
+#       https://explainshell.com/explain/1/rsync
+# GITHUB: https://github.com/PoissonChevre/rsync-scripts_P10
+# -----------------------------------------------------------------------------
+
+# Check for required arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <TARGET_DIR> <RETENTION>"
+    exit 1
+fi
+
+# Assign arguments to variables
+TARGET_DIR="$1"
+RETENTION="$2"
+SRC_DIR="/home/rsync_adm/$TARGET_DIR/"
+DST_DIR="/home/rsync_adm/backup_storage/$TARGET_DIR/"
+REMOTE="rsync_adm@backup-srv"
+TIMESTAMP=$(date +%Y%m%d_%H%M)
+PARAMETERS=(
+    -a      # --archive, equivalent to -rlptgoD (--recursive;--links;--perms;--times;--group;--owner;equivalent to --devices & --specials)
+    -v      # --verbose
+ #   -q     # --quiet (better with cron) ==> verbose mode for the demo
+    -P      # equivalent to --partial --progress
+    -e ssh  # ssh remote
+    --log-file=/var/log/rsync/incr_backup.log # path to the log file
+)
+
+# Find the last  backup or will create one if there is no  backup found
+is_backup_existing() {
+    LAST_INCR_BACKUP=$(ssh $REMOTE "ls -d ${DST_DIR}backup_incr_* 2>/dev/null | sort | tail -n 1")
+    if [ -n "$LAST_INCR_BACKUP" ]; then
+        # Display last  backup path
+        echo "Last incremental backup: $LAST_INCR_BACKUP"
+        return 0 # 0 = true
+    else
+        # No existing full backup found, we will force the creation of one
+        echo "No previous backup found."
+        return 1 # 1 = false
+    fi
+}
+
+perform_full_backup() {
+    echo "Creating a new full backup..."
+    rsync "${PARAMETERS[@]}" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_incr_${TIMESTAMP}"
+    # Call the fct to display the directory path of the new full backup 
+    is_backup_existing
+ }
+
+# Perform an incremental backup
+perform_incr_backup() {
+    if is_backup_existing; then
+            # Differential backup using the most recent backup as reference
+            echo "Performing incremental backup using the most recent backup as reference."
+            rsync "${PARAMETERS[@]}" --link-dest="$LAST_INCR_BACKUP" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_incr_${TIMESTAMP}"
+    else
+        #  No existing full backup found, forcing the creation of a new one
+        echo "Forcing the creation of a new full backup..."
+        perform_full_backup
+    fi
+}
+# Clean up old backups, keeping only backups from the last N days and
+# ensuring the two most recent backups are retained regardless of age.
+cleanup_old_backups() {
+    echo "Cleaning up old backups..."
+    # Remove old backups  based on RETENTION days (using ctime for directory change time)
+    ssh $REMOTE "find $DST_DIR -maxdepth 1 -type d -name 'backup_incr_*' -ctime +$RETENTION -exec rm -rf {} \;"
+}
+
+# Main execution flow
+main() {
+    echo "Starting backup process..."
+    perform_incr_backup
+    cleanup_old_backups
+    echo "Backup and cleanup completed."
+}
+
+# Execute the main function
+main
--- incr_backup.sh
+++ incr_backup.sh
@@ -6,85 +6,135 @@
 # Author: Renaud CHARLIER
 # Date: 18-02-2024
 # Description: Performs an incremental backup using rsync, keeping backups
-# from the specified number of days and ensuring at least the two most recent
+# from the specified number of days and ensuring the most recent
 # backups are always retained.
-# Usage: ./backup_script.sh <TARGET_DIR> <RETENTION>
-# Example: ./backup_script.sh TICKETS 7
+# Usage: ./backup_script.sh <TARGET_DIR> <DAY_FULL_BACKUP> <RETENTION> 
+# Example: ./backup_script.sh TICKETS 7 7
 # Note: Ensure SSH password-less authentication is set up for rsync_adm@backup-srv.
 #       https://explainshell.com/explain/1/rsync
 # GITHUB: https://github.com/PoissonChevre/rsync-scripts_P10
 # -----------------------------------------------------------------------------
 
 # Check for required arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <TARGET_DIR> <RETENTION>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <TARGET_DIR> <DAY_FULL_BACKUP> <RETENTION> "
     exit 1
 fi
 
 # Assign arguments to variables
 TARGET_DIR="$1"
-RETENTION="$2"
+# Day of the week for the full backup Mon=1 ==> Sun=7
+DAY_FULL_BACKUP="$2"
+RETENTION="$3"
+# TARGET_DIR FICHIERS | MAILS | MACHINES | RH | SITE | TICkETS
 SRC_DIR="/home/rsync_adm/$TARGET_DIR/"
 DST_DIR="/home/rsync_adm/backup_storage/$TARGET_DIR/"
-REMOTE="rsync_adm@backup-srv"
+REMOTE="rsync_adm@backup-srv" 
 TIMESTAMP=$(date +%Y%m%d_%H%M)
 PARAMETERS=(
-    -a      # --archive, equivalent to -rlptgoD (--recursive;--links;--perms;--times;--group;--owner;equivalent to --devices & --specials)
-    -v      # --verbose
- #   -q     # --quiet (better with cron) ==> verbose mode for the demo
-    -P      # equivalent to --partial --progress
-    -e ssh  # ssh remote
+    -a              # --archive, equivalent to -rlptgoD (--recursive;--links;--perms;--times;--group;--owner;equivalent to --devices & --specials)
+    -v              # --verbose
+    -P              # equivalent to --partial --progress
+    -e ssh          # ssh remote
     --log-file=/var/log/rsync/incr_backup.log # path to the log file
+ #   -q             # --quiet (better with cron) ==> verbose mode for the demo
 )
 
-# Find the last  backup or will create one if there is no  backup found
-is_backup_existing() {
-    LAST_INCR_BACKUP=$(ssh $REMOTE "ls -d ${DST_DIR}backup_incr_* 2>/dev/null | sort | tail -n 1")
-    if [ -n "$LAST_INCR_BACKUP" ]; then
-        # Display last  backup path
-        echo "Last incremental backup: $LAST_INCR_BACKUP"
+# Find the last full backup or will create one if there is no full backup found
+is_full_backup_existing() {
+    LAST_FULL_BACKUP=$(ssh $REMOTE "ls -d ${DST_DIR}backup_FULL_* 2>/dev/null | sort | tail -n 1")
+    if [ -n "$LAST_FULL_BACKUP" ]; then
+        # Display last full backup path
+        echo "Last full backup: $LAST_FULL_BACKUP"
         return 0 # 0 = true
     else
         # No existing full backup found, we will force the creation of one
+        echo "No previous full backup found."
+        return 1 # 1 = false
+    fi
+}
+
+# Check if the last full backup is older than the specified RETENTION (return boolean)
+is_last_full_backup_old() {
+    CURRENT_TIME=$(date +%s)
+    LAST_BACKUP_TIME=$(ssh $REMOTE "stat -c %W $LAST_FULL_BACKUP")
+    ELAPSED_TIME=$((CURRENT_TIME - LAST_BACKUP_TIME))
+    # Converting RETENTION in seconds ==> one day =24*3600s=86400s
+    RETENTION_IN_SECONDS=$((RETENTION * 86400))
+    if [ "$ELAPSED_TIME" -ge "$RETENTION_IN_SECONDS" ]; then 
+        # Last full backup is older than RETENTION days 
+        return 0  # 0 = true
+    else
+        # Last full backup is within the RETENTION days
+        return 1  # 1 = false
+    fi
+}
+
+# Find the most recent backup
+is_last_backup() {
+    LAST_BACKUP=$(ssh $REMOTE "ls -d ${DST_DIR}backup_* 2>/dev/null | sort | tail -n 1")
+    if [ -n "$LAST_FULL_BACKUP" ]; then
+        # Display last backup path
+        echo "Last backup: $LAST_BACKUP"
+        return 0 # 0 = true
+    else
+        # No backup found
         echo "No previous backup found."
         return 1 # 1 = false
     fi
 }
 
+# Clean up old incremental backups
+cleanup_old_incr_backups() {
+    echo "Cleaning up old incremental backups..."
+    ssh $REMOTE "find $DST_DIR -maxdepth 1 -name 'backup_incr*' -ctime +$RETENTION -exec rm -rf {} \;"
+}
+
+# Clean up old FULL backups
+cleanup_old_full_backups() {
+    echo "Cleaning up old full backups..."
+    # Remove old backups  based on RETENTION days (using ctime for directory change time)
+    ssh $REMOTE "find $DST_DIR -maxdepth 1 -name 'backup_FULL*' -ctime +$RETENTION -exec rm -rf {} \;"
+}
+
+# Perform a full backup
 perform_full_backup() {
     echo "Creating a new full backup..."
-    rsync "${PARAMETERS[@]}" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_incr_${TIMESTAMP}"
+    rsync "${PARAMETERS[@]}" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_FULL_${TIMESTAMP}"
     # Call the fct to display the directory path of the new full backup 
-    is_backup_existing
+    is_full_backup_existing
  }
 
-# Perform an incremental backup
+# Perform a differential backup
 perform_incr_backup() {
-    if is_backup_existing; then
-            # Differential backup using the most recent backup as reference
+    if is_full_backup_existing; then
+        # Last full backup is older than RETENTION days and  today is DAY_FULL_BACKUP, create a new full backup
+        if is_last_full_backup_old && [ "$(date +%u)" -eq $DAY_FULL_BACKUP ]; then
+            echo "Last full backup is older than $RETENTION days." 
+            perform_full_backup
+            cleanup_old_full_backups
+            cleanup_old_incr_backups
+        else
+            # Incremental backup using the most recent backup as reference
+            is_last_backup
             echo "Performing incremental backup using the most recent backup as reference."
-            rsync "${PARAMETERS[@]}" --link-dest="$LAST_INCR_BACKUP" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_incr_${TIMESTAMP}"
+            rsync "${PARAMETERS[@]}" --link-dest="$LAST_BACKUP" "$SRC_DIR" "$REMOTE:${DST_DIR}backup_incr_${TIMESTAMP}"
+         fi
     else
         #  No existing full backup found, forcing the creation of a new one
         echo "Forcing the creation of a new full backup..."
         perform_full_backup
+        cleanup_old_incr_backups
     fi
-}
-# Clean up old backups, keeping only backups from the last N days and
-# ensuring the two most recent backups are retained regardless of age.
-cleanup_old_backups() {
-    echo "Cleaning up old backups..."
-    # Remove old backups  based on RETENTION days (using ctime for directory change time)
-    ssh $REMOTE "find $DST_DIR -maxdepth 1 -type d -name 'backup_incr_*' -ctime +$RETENTION -exec rm -rf {} \;"
 }
 
 # Main execution flow
 main() {
     echo "Starting backup process..."
     perform_incr_backup
-    cleanup_old_backups
     echo "Backup and cleanup completed."
 }
 
 # Execute the main function
 main
+
