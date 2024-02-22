34036a2722df69f82468291d46d6c7e6a9bcaabb
Renaud <charlierrenaud@hotmail.com>
2024-02-22 19:26:39 +0200

    updates

--- incr_backup.sh
+++ incr_backup.sh
@@ -72,8 +72,7 @@ is_last_full_backup_old() {
 
 # Find the most recent backup (using mtime for directory change time), create less than 1620 minutes (27 heures)
 is_last_backup() {
-    LAST_BACKUP=$(ssh $REMOTE "find $DST_DIR -maxdepth 1 -name 'backup_*' -cmin -100;")
-    if [ -n "$LAST_FULL_BACKUP" ]; then
+    LAST_BACKUP=$(ssh $REMOTE "find $DST_DIR -maxdepth 1 -name 'backup_*' -cmin 100")
         # Display last backup path
         echo "Last backup: $LAST_BACKUP"
         return 0 # 0 = true
